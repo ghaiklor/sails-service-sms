46f0df8f9ba799a0f9a62e3f18a2a43bb58c8340
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-23 14:37:15 +0300

    Add tests for SmsService

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,21 @@
 var assert = require('chai').assert;
 var SmsService = require('../');
+var TwilioSms = SmsService.TwilioSms;
 
 describe('SmsService', function () {
   it('Should properly export', function () {
     assert.isObject(SmsService);
     assert.isFunction(SmsService.create);
-    assert.isFunction(SmsService.Twilio);
+    assert.isFunction(SmsService.TwilioSms);
+  });
+
+  it('Should properly create twilio instance', function () {
+    assert.instanceOf(SmsService.create('twilio'), TwilioSms);
+  });
+
+  it('Should properly throw exception on create unrecognized', function () {
+    assert.throw(function () {
+      SmsService.create('NOT_EXISTS');
+    }, Error);
   });
 });
