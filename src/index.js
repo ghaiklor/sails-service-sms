be36a79dc3c3892a64f899f6d5bbac4f846daf54
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 13:40:04 +0300

    Rewrite SmsService with ES6

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-var sms = {
+import TwilioSms from './TwilioSms';
+
+const sms = {
   twilio: require('./TwilioSms')
 };
 
-module.exports = {
-  /**
-   * Create instance of SMS service
-   * @param {String} type
-   * @param {Object} config
-   * @returns {*}
-   */
-  create: function (type, config) {
-    if (sms[type.toLowerCase()] instanceof Function) {
-      return new sms[type.toLowerCase()](config);
-    } else {
-      throw new Error('Unrecognized type');
-    }
-  },
-
-  TwilioSms: sms.twilio
-};
+/**
+ * Create instance of SMS service
+ * @param {String} type
+ * @param {Object} config
+ * @returns {*}
+ */
+export default function (type, config) {
+  if (sms[type.toLowerCase()] instanceof Function) {
+    return new sms[type.toLowerCase()](config);
+  } else {
+    throw new Error('Unrecognized type -> ' + type);
+  }
+}
