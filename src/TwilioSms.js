b4044bcbd3030db0dff1d7ab9570e826f83ddcea
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 13:32:55 +0300

    Rewrite TwilioSms with ES6

--- src/TwilioSms.js
+++ src/TwilioSms.js
@@ -1,52 +1,34 @@
-var util = require('util');
-var _ = require('lodash');
-var Promise = require('bluebird');
-var twilio = require('twilio');
-var BaseSms = require('./BaseSms');
+import _ from 'lodash';
+import Promise from 'bluebird';
+import twilio from 'twilio';
+import BaseSms from './BaseSms';
 
-util.inherits(TwilioSms, BaseSms);
+export default class TwilioSms extends BaseSms {
+  constructor(...args) {
+    super(...args);
 
-/**
- * Create new Twilio SMS
- * @constructor
- */
-function TwilioSms() {
-  BaseSms.apply(this, arguments);
-
-  this.setProvider(twilio(this.get('provider.accountSid'), this.get('provider.authToken')));
-}
-
-/**
- * Send message to the recipient
- * @returns {Promise}
- * @private
- */
-TwilioSms.prototype._send = function (config) {
-  return new Promise(function (resolve, reject) {
-    this.getProvider().messages.create(config, function (error, message) {
-      return error ? reject(error) : resolve(message);
-    });
-  }.bind(this));
-};
-
-/**
- * Send message
- * @param {Object} [_config] Additional configuration
- * @returns {Promise}
- */
-TwilioSms.prototype.send = function (_config) {
-  var config = _.merge({}, _.omit(this.get(), 'provider'), _config);
-  var promises = [];
-
-  for (var i = 0; i < config.recipient.length; i++) {
-    promises.push(this._send({
-      from: config.sender,
-      to: config.recipient[i],
-      body: config.message
-    }));
+    this.setProvider(twilio(this.get('provider.accountSid'), this.get('provider.authToken')));
   }
 
-  return Promise.all(promises);
-};
+  /**
+   * Send message
+   * @param {Object} [_config] Additional configuration
+   * @returns {Promise}
+   */
+  send(_config) {
+    let config = _.merge({}, _.omit(this.get(), 'provider'), _config);
+    let promises = [];
 
-module.exports = TwilioSms;
+    for (let i = 0; i < config.recipient.length; i++) {
+      promises.push(new Promise((resolve, reject) => {
+        this.getProvider().messages.create({
+          from: config.sender,
+          to: config.recipient[i],
+          body: config.message
+        }, (error, message) => error ? reject(error) : resolve(message));
+      }));
+    }
+
+    return Promise.all(promises);
+  }
+}
