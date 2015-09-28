9dfaf5f31cbdb5d272b00fd50724c6e513a20e9f
Eugene Obrezkov <ghaiklor@gmail.com>
2015-09-28 13:20:07 +0300

    Rewrite BaseSms with ES6

--- src/BaseSms.js
+++ src/BaseSms.js
@@ -1,56 +1,48 @@
-var _ = require('lodash');
+import _ from 'lodash';
 
-/**
- * Create base instance for SMS
- * @param {Object} [_config]
- * @constructor
- */
-function BaseSms(_config) {
-  this._config = {};
+export default class BaseSms {
+  constructor(_config) {
+    this._config = {};
+    this._provider = {};
 
-  _.forOwn(_config, function (value, key) {
-    this.set(key, value);
-  }.bind(this));
+    _.assign(this._config, _config);
+  }
+
+  /**
+   * Get configuration value
+   * @param {String} [path]
+   * @returns {*}
+   */
+  get(path) {
+    return typeof path === 'undefined' ? this._config : _.get(this._config, path);
+  }
+
+  /**
+   * Set configuration value
+   * @param {String} path
+   * @param {*} value
+   * @returns {BaseSms}
+   */
+  set(path, value) {
+    _.set(this._config, path, value);
+    return this;
+  }
+
+  /**
+   * Get provider for sending notifications
+   * @returns {*}
+   */
+  getProvider() {
+    return this._provider;
+  }
+
+  /**
+   * Set new provider to this pusher
+   * @param {*} provider
+   * @returns {BaseSms}
+   */
+  setProvider(provider) {
+    this._provider = provider;
+    return this;
+  }
 }
-
-/**
- * Get configuration value
- * @param {String} [path]
- * @returns {*}
- */
-BaseSms.prototype.get = function (path) {
-  return typeof path === 'undefined' ? this._config : _.get(this._config, path);
-};
-
-/**
- * Set configuration value
- * @param {String} path
- * @param {*} value
- * @returns {BaseSms}
- */
-BaseSms.prototype.set = function (path, value) {
-  _.set(this._config, path, value);
-  return this;
-};
-
-/**
- * Get provider for sending notifications
- * @returns {*}
- */
-BaseSms.prototype.getProvider = function () {
-  return this._provider;
-};
-
-/**
- * Set new provider to this pusher
- * @param {*} provider
- * @returns {BaseSms}
- */
-BaseSms.prototype.setProvider = function (provider) {
-  this._provider = provider;
-  return this;
-};
-
-BaseSms.prototype.send = _;
-
-module.exports = BaseSms;
